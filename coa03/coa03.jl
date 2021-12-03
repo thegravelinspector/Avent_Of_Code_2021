d2f183af19de17dba10424c774a26d2cde9b69ee
Micael Karlsson <micca@micca.com>
2021-12-03 15:35:16 +0100

    day 3 part 2

--- coa03/coa03.jl
+++ coa03/coa03.jl
@@ -4,9 +4,7 @@
 
 using StaticArrays
 
-diagnostics = readlines("input.txt")
-
-function coa03(diagnostics)
+function coa03_part1(diagnostics)
     rate = SVector(repeat([0], length(diagnostics[1]))...)
     len = 0
     for diag in diagnostics
@@ -20,6 +18,34 @@ end
 
 # Part 2
 
+function prune!(diagnostics, bitno, reference_level::Bool)
+    rate, len = 0, 0
+    for diag in diagnostics
+        rate += diag[bitno] == '1'
+        len += 1
+    end
+    mostcommon = ((rate >= len-rate)⊻!reference_level) ? '1' : '0'
+    filter!(data->data[bitno]==mostcommon, diagnostics)
+end
+
+function diagnose(data, reference_level::Bool)
+    bitno, data = 1, copy(data)
+    while length(data) > 1
+        prune!(data, bitno, reference_level)
+        bitno += 1
+    end
+    parse(Int, data[1], base=2)
+end
+
+function aoc03_part2(test_data)
+    oxygen_generator_rating = diagnose(test_data, true)
+    CO2_scrubber_rating = diagnose(test_data, false)
+    oxygen_generator_rating * CO2_scrubber_rating
+end
+
 # Run diagnostics
 
-@show coa03(diagnostics)
+input = readlines("input.txt")
+
+@show coa03_part1(input)
+@show aoc03_part2(input)
