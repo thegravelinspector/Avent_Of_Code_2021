3b6c4017425514d5e1284dd1b414de70de2428dc
Micael Karlsson <micca@micca.com>
2021-12-04 20:41:07 +0100

    on the point

--- coa04/coa04.jl
+++ coa04/coa04.jl
@@ -2,7 +2,7 @@
 
 using DelimitedFiles, StaticArrays
 
-# Part 1: Cheat the squid
+# Part 1: Cheat the squid and lose the sub
 
 get_numbers(fname) = parse.(Int, split(readline(fname), ','))
 get_boards(fname) = readdlm(fname, Int, skipstart=2)
@@ -23,56 +23,37 @@ function is_bingo!(board, number)
     board[:,ix[2]] == BINGO
 end
 
-function choose_board(numbers, boards)
-    for n in numbers
-        for bix in 1:5:size(boards, 1)
-            board = @view boards[bix:bix+4,:]
-            is_bingo!(board, n) && return board, n
-        end
-    end
-    error("The sub sank!")
-end
-
 winnings(board) = sum(board[board .> 0])
 
-function profit(tombola)
-    winning_board, number = choose_board(tombola...)
-    winnings(winning_board) * number
+function first_bingo_score(numbers, boards)
+    for n in numbers, bix in 1:5:size(boards, 1)
+        board = @view boards[bix:bix+4,:]
+        is_bingo!(board, n) && return winnings(board) * n
+    end
+    error("The sub sank anyway!")
 end
 
-coa04_part1(fname) = profit(get_tombola(fname))
+coa04_part1(fname) = first_bingo_score(get_tombola(fname)...)
 
 # Part 2: Save the sub
 
-function choose_board_in_favor_of_squid(numbers, boards)
-    remove!(board) = board[1,1] = -board[1,1] - 3
+function last_bingo_score(numbers, boards)
+    remove!(board, n) = board[1,1] = -2
     isremoved_board(boards, bix) = boards[bix,1] <= -2
-    restore!(board) = board[1,1] = -(board[1,1] + 3)
 
-    last_bingo_board_ix = 0
-    last_winning_number = 0
-    for n in numbers
-        for bix in 1:5:size(boards, 1)
-            isremoved_board(boards, bix) && continue
-            board = @view boards[bix:bix+4,:]
-            if is_bingo!(board, n)
-                remove!(board)
-                last_bingo_board_ix = bix
-                last_winning_number = n
-            end
+    last_bingo_score = 0
+    for n in numbers, bix in 1:5:size(boards, 1)
+        isremoved_board(boards, bix) && continue
+        board = @view boards[bix:bix+4,:]
+        if is_bingo!(board, n)
+            remove!(board, n)
+            last_bingo_score = winnings(board) * n
         end
     end
-    board = @view boards[last_bingo_board_ix:last_bingo_board_ix+4,:]
-    restore!(board)
-    board, last_winning_number
+    last_bingo_score
 end
 
-function save_the_sub(tombola)
-    losing_board, number = choose_board_in_favor_of_squid(tombola...)
-    winnings(losing_board) * number
-end
-
-coa04_part2(fname) = save_the_sub(get_tombola(fname))
+coa04_part2(fname) = last_bingo_score(get_tombola(fname)...)
 
 # Cheat or not to cheat
 
