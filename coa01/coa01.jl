00709db7927b7aea3ca0d052a566d8277c3df66d
Micael Karlsson <micca@micca.com>
2021-12-05 08:06:14 +0100

    bench them

--- coa01/coa01.jl
+++ coa01/coa01.jl
@@ -3,14 +3,22 @@
 fname = "input01.txt"
 input = parse.(Int, readlines(fname))
 
-coa01a(data) = foldl(1:length(data)-1) do acc,i
+coa01_part1(data) = foldl(1:length(data)-1) do acc,i
      @inbounds acc += data[i] < data[i+1]
 end
 
-coa01b(data) = foldl(1:length(data)-3) do acc,i
+coa01_part2(data) = foldl(1:length(data)-3) do acc,i
     @inbounds acc += data[i] < data[i+3]
 end
 
-@show coa01a(input)
-@show coa01b(input);
+@show coa01_part1(input)
+@show coa01_part2(input)
 
+using BenchmarkTools
+
+# Without slow disk IO
+function benchit(input)
+    coa01_part1(input), coa01_part2(input)
+end
+
+@btime benchit(input)
