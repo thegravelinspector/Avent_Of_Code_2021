b33df2cf641dc9f706e32efde356ad69a13a966c
Micael Karlsson <micca@micca.com>
2021-12-05 21:41:11 +0100

    structs , good riddance

--- coa05/coa05.jl
+++ coa05/coa05.jl
@@ -1,27 +1,21 @@
-struct Line
-    x1::Int16; y1::Int16; x2::Int16; y2::Int16
-end
-
-ishor(l::Line) = l.y1==l.y2
-isvert(l::Line) = l.x1==l.x2
+# Code of Ad(verse) Vent(s)
 
 function get_vent_lines(data)
     max_reach = 0
-    lines = Vector{Line}()
-    for datapoint in data
-        x1, y1, x2, y2 = parse.(Int, SubString.(datapoint, findall(r"(\d+)", datapoint)))
-        max_reach = max(max_reach, x1, x2, y1, y2)
-        push!(lines, Line(x1,y1,x2,y2))
+    lines = Matrix{Int}(undef, length(data), 4)
+    for (i,datapoint) in enumerate(data)
+        lines[i,:] = parse.(Int, SubString.(datapoint, findall(r"(\d+)", datapoint)))
+        max_reach = max(max_reach, maximum(@view lines[i,:]))
     end
     lines, max_reach
 end
 
-new_diagram(size) = zeros(Int8, size+1, size+1)
+new_diagram(size) = zeros(Int8, size, size)
 
-function mark!(diagram, l)
-    i, j = l.y1+1, l.x1+1
-    Δi, Δj = sign(l.y2-l.y1), sign(l.x2-l.x1)
-    for _ in 0:max(abs(l.x2-l.x1), abs(l.y2-l.y1))
+function mark!(diagram, x1, y1, x2, y2)
+    i, j = y1+1, x1+1
+    Δi, Δj = sign(y2-y1), sign(x2-x1)
+    for _ in 0:max(abs(x2-x1), abs(y2-y1))
         diagram[i, j] += 1
         i += Δi
         j += Δj
@@ -29,25 +23,25 @@ function mark!(diagram, l)
 end
 
 function coa05_part1!(diagram, vent_lines)
-    for vl in vent_lines
-        (ishor(vl)||isvert(vl)) && mark!(diagram, vl)
+    for (x1, y1, x2, y2) in eachrow(vent_lines)
+        (y1 == y2 || x1 == x2) && mark!(diagram, x1, y1, x2, y2)
     end
     count(>(1), diagram)
 end
 
 function coa05_part2!(diagram, vent_lines)
-    for vl in vent_lines
-        !(ishor(vl)||isvert(vl)) && mark!(diagram, vl)
+    for (x1, y1, x2, y2) in eachrow(vent_lines)
+        !(y1 == y2 || x1 == x2) && mark!(diagram, x1, y1, x2, y2)
     end
     count(>(1), diagram)
 end
 
 input = readlines("input.txt")
 
-# Danger level of vent zones
+# Navigate
 
 vent_lines, max_reach = get_vent_lines(input)
-diagram = new_diagram(max_reach)
+diagram = new_diagram(max_reach+1)
 
 @show coa05_part1!(diagram, vent_lines)
 @show coa05_part2!(diagram, vent_lines)
@@ -58,7 +52,7 @@ using BenchmarkTools
 
 function benchit(input)
     vent_lines, max_reach = get_vent_lines(input)
-    diagram = new_diagram(max_reach)
+    diagram = new_diagram(max_reach+1)
 
     coa05_part1!(diagram, vent_lines),
     coa05_part2!(diagram, vent_lines)
