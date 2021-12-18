709fab86bc65a71b3bd3cc981c949a954bf1b825
Micael Karlsson <micca@micca.com>
2021-12-18 14:19:39 +0100

    day 15 removed visualisations

--- coa15/coa15.jl
+++ coa15/coa15.jl
@@ -1,7 +1,5 @@
 # Code of Advent
 
-using UnicodePlots, BenchmarkTools
-
 vcl(f,x1) = x2->f.(x1,x2)
 vcr(f,x2) = x1->f.(x1,x2)
 
@@ -56,68 +54,6 @@ function get_min_costs(A)
     S
 end
 
-function trace(S; start=nothing, stop=nothing, max_iter=nothing)
-    max_cost = maximum(S)
-    sr, sc = size(S)
-
-    isnothing(start) && (start = [sr, sc])
-    isnothing(stop) && (stop = [1, 1])
-    isnothing(max_iter) && (max_iter = sr*sc)
-
-    TT, TT_done = [[start]], []
-
-    directions = [[-1,0], [1,0], [0,-1], [0,1]]
-    SS(t) = 0<t[1]<=sr && 0<t[2]<=sc ? S[t[1],t[2]] : max_cost+1
-
-    i = 1
-    while i <= length(TT)
-        T = TT[i]
-        old_waypoint = copy(start)
-        waypoint = start
-        max_iter_count_down = 0
-
-        while waypoint != stop
-            next_possible_waypoints = [waypoint .+ dir for dir in directions]
-            ball = SS.(setdiff!(next_possible_waypoints, T))
-            isempty(ball) && @goto end_all
-            cost, index = findmin(ball)
-            a = findall(isequal(cost), ball)
-            if length(a) > 1
-                for i in a[2:end]
-                    next_wp = next_possible_waypoints[i]
-                    if next_wp ∉ T && ~isempty(next_wp)
-                        new_T = copy(T)
-                        push!(new_T, next_wp)
-                        push!(TT, new_T)
-                    end
-                end
-            end
-            next_waypoint = next_possible_waypoints[index]
-            if SS(next_waypoint) < max_cost && max_iter_count_down < max_iter && maximum(abs.((T[end] .- next_waypoint))) <= 1
-                push!(T, next_waypoint)
-                old_waypoint = copy(waypoint)
-                waypoint = next_waypoint
-                max_iter_count_down += 1
-            else
-                push!(TT_done, T)
-                break
-            end
-        end
-        i += 1
-    end
-    @label end_all
-    TT
-end
-
-function show_trace(S, TT=trace(S))
-    P = heatmap(S[end:-1:1,:], color=:grays)
-    for T in TT
-        line_width =  maximum(size(S)) < 20 ? 2 : 1
-        P = plot!(P,last.(T), size(S,1) .- first.(T) .+ 1, lw=line_width, color=:red, leg=:none)
-    end
-    P
-end
-
 lowest_risk(costs) = costs[end,end] - costs[1,1]
 
 coa15_part1(cavern) = (;lowest_risk=lowest_risk(get_min_costs(cavern)))
@@ -127,6 +63,3 @@ cavern = get_cavern("input.txt")
 
 @show coa15_part1(cavern)
 @show coa15_part2(cavern)
-
-#plot(show_trace(get_min_costs(cavern)),
-#     show_trace(get_min_costs(large_map(cavern))), size=(800,200))
