20a0d0a616e389cc1ec4278fad8c99d6c4138b81
Micael Karlsson <micca@micca.com>
2022-03-04 21:45:58 +0100

    day 25

--- coa25/coa25.jl
+++ coa25/coa25.jl
@@ -5,21 +5,21 @@ function get_trench(fname)
     cucumbers = hcat([first.(split(line, "")) for line in lines]...)
     trench = fill('.', size(cucumbers) .+ (1,1))
     trench[1:size(cucumbers,1),1:size(cucumbers,2)] .= cucumbers
-    trench
+    Int8.(trench)
 end
 
-function step!(trench)
-    not_done = false
+function move!(trench, dir)
     rows, cols = size(trench) .- (1,1)
-
     r, c = 1, 1
+    not_done = false
+
     trench[end,:] .= @view trench[1,:]
     while true
-        if trench[r, c] == '>'
+        if trench[r, c] == dir
             next_r = r + 1
-            if trench[next_r, c] == '.'
-                trench[r, c] = '.'
-                trench[mod1(next_r, rows), c] = '>'
+            if trench[next_r, c] == Int8('.')
+                trench[r, c] = Int8('.')
+                trench[mod1(next_r, rows), c] = dir
                 r += 1
                 not_done = true
             end
@@ -33,29 +33,12 @@ function step!(trench)
             end
         end
     end
-
-    r, c = 1, 1
-    trench[:,end] .= @view trench[:,1]
-    while true
-        if trench[r, c] == 'v'
-            next_c = c + 1
-            if trench[r, next_c] == '.'
-                trench[r, c] = '.'
-                trench[r, mod1(next_c, cols)] = 'v'
-                c += 1
-                not_done = true
-            end
-        end
-        c += 1
-        if c > cols
-            c = 1
-            r += 1
-            if r > rows
-                break
-            end
-        end
-    end
     not_done
+end
+
+function step!(trench)
+    not_done = move!(trench, Int8('>'))
+    move!(transpose(trench), Int8('v')) || not_done
 end
 
 function coa25(trench)
@@ -67,4 +50,8 @@ function coa25(trench)
 end
 
 trench = get_trench("input.txt")
+
+using BenchmarkTools
+
 @show coa25(trench);
+@btime coa25(trench)
